138aaed815b795cb18d293d9cd146063b1d7060d
Ujual P S <82148053+ujualps-deloitte@users.noreply.github.com>
2022-03-29 11:57:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import moment from "moment";
+import "./App.css";
 
 function App() {
+  const [expiryTime, setExpiryTime] = useState(null);
+  const dateTimeRef = useRef(null);
+  const expTimeRef = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dateTimeRef.current.innerText = `Current time is ${moment().format(
+        "hh : mm : ss"
+      )}`;
+      const timeRemaining = moment.duration(expiryTime - moment.now());
+      expTimeRef.current.innerText = expiryTime
+        ? `${timeRemaining.hours()} Hours ${timeRemaining.minutes()} Minutes ${timeRemaining.seconds()} Seconds`
+        : null;
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  const btnClick = () => {
+    if (expiryTime) {
+      expiryTime.add(10, "minutes");
+    } else {
+      setExpiryTime(moment().add(10, "minutes"));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <span ref={dateTimeRef} />
+      <br />
+      <span ref={expTimeRef} />
+      <br />
+      <button onClick={btnClick}>Add Time</button>
     </div>
   );
 }
